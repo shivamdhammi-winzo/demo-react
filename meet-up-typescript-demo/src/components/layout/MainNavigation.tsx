917beb41a369bd1d5b06e41b128b5d614476c283
Shivam Dhammi <shivam.dhammi@winzogames.com>
2021-06-11 05:44:22 +0530

    meet-up-typescript-demo partially-completed

--- meet-up-typescript-demo/src/components/layout/MainNavigation.tsx
+++ meet-up-typescript-demo/src/components/layout/MainNavigation.tsx
@@ -3,10 +3,22 @@ import classes from "./MainNavigation.module.css";
 
 const MainNavigation: React.FC = (props) => {
   return (
-    <div>
-      <MainNavigation />
-      <main className={classes.main}>{props.children}</main>
-    </div>
+    <header className={classes.header}>
+      <div className={classes.logo}>React Meetups</div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">All Meetups</Link>
+          </li>
+          <li>
+            <Link to="/new-meetup">Add New Meetups</Link>
+          </li>
+          <li>
+            <Link to="/favorites">My Favorites</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
